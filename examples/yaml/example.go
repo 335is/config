1ad728a9b9a568544b875e7a9f8be890fe60753b
OPTIPLEX7010\Scott <tornacious@gmail.com>
2021-02-04 00:23:14 -0800

    Added another YAML file example to show nested structs.

--- examples/yaml/example.go
+++ examples/yaml/example.go
@@ -7,14 +7,23 @@ import (
 	config "github.com/335is/config"
 )
 
-type cfg struct {
+type cfg1 struct {
 	Address string        `yaml:"address"`
 	Timeout time.Duration `yaml:"timeout"`
 }
 
+type cfg2 struct {
+	HTTP cfg1
+}
+
 func main() {
-	c := cfg{}
-	config.Load("MYAPP", "./cfg.yml", &c)
-	s, _ := config.ToYaml(&c)
-	fmt.Printf("%s\n", s)
+	c1 := cfg1{}
+	config.Load("MYAPP1", "./cfg1.yml", &c1)
+	s1, _ := config.ToYaml(&c1)
+	fmt.Printf("%s\n", s1)
+
+	c2 := cfg2{}
+	config.Load("MYAPP2", "./cfg2.yml", &c2)
+	s2, _ := config.ToYaml(&c2)
+	fmt.Printf("%s\n", s2)
 }
